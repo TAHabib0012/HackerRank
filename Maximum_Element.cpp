3cf0ed900ade9f4d07966e08520f78113afd4bd6
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-30 20:46:05 +0600

    Solved

---
+++ Maximum_Element.cpp
@@ -0,0 +1,58 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+vector<int> a;
+void ans()
+{
+    LL n, c;
+    cin >> n;
+
+    if (n == 1)
+    {
+        LL x;
+        cin >> x;
+        a.push_back(x);
+    }
+    else if (n == 2)
+    {
+        a.pop_back();
+    }
+    else if (n == 3)
+    {
+        cout << *max_element(a.begin(), a.end()) << endl;
+    }
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
