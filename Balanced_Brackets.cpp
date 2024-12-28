835d9c32c40f7ca93b1c9bfb89b6cfcca5c8352f
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-12-29 04:47:01 +0600

    Solved

---
+++ Balanced_Brackets.cpp
@@ -0,0 +1,88 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    string s;
+    cin >> s;
+    stack<char> st;
+
+    LL n = s .size();
+
+    if (n <= 1)
+    {
+        no;
+        return;
+    } 
+    
+    for (LL i = 0; i < n; i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            st.push(s[i]);
+        }
+        
+        else
+        {
+            if (st.empty())
+            {
+                st.push(s[i]);
+            }
+            
+            else if (st.top() == '(' && s[i] == ')' || st.top() == '{' && s[i] == '}' || st.top() == '[' && s[i] == ']')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+    }
+    
+    if (st.empty())
+    {
+        yes;
+    }
+    else
+    {
+        no;
+    }
+    
+
+
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
